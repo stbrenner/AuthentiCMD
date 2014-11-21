16b74c3588a86adce8e083e883da6f93f9db0ae2
Stephan Brenner <ymx@brennerweb.com>
2014-11-21 15:11:50 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AuthentiCMD
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			try
+			{
+				var cert = new X509Certificate(args[0]);
+				Console.WriteLine(cert.GetPublicKeyString());
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AuthentiCMD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Stephan Brenner")]
+[assembly: AssemblyProduct("AuthentiCMD")]
+[assembly: AssemblyCopyright("© Stephan Brenner 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("15ff59b6-4ca9-41be-a2eb-da39491d71a5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
